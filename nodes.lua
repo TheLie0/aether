b7027c31cc91967439dd4918a0044e224ab23147
Dannieboy <dannieboy@localhost.localdomain>
2020-09-29 22:54:23 +0200

    Now using Aeuther 1 assets

--- nodes.lua
+++ nodes.lua
@@ -4,20 +4,20 @@
 --Dirts
 minetest.register_node("aether2:quicksoil", {
   description = 'Quick Soil',
-  tiles = {"quicksoil.png"},
+  tiles = {"blocks/quicksoil.png"},
   groups = {crumbly = 1},
 })
 
 minetest.register_node("aether2:aether_dirt", {
   description = 'Aether Dirt',
-  tiles = {"aether_dirt.png"},
+  tiles = {"blocks/aether_dirt.png"},
   groups = {crumbly = 1},
 })
 
 minetest.register_node("aether2:aether_dirt_with_grass", {
 	description = "Aether Grass",
-	tiles = {"aether_grass_top.png", "aether_dirt.png",
-		{name = "aether_grass_side.png",
+	tiles = {"blocks/aether_grass_top.png", "blocks/aether_dirt.png",
+		{name = "blocks/aether_grass_side.png",
 			tileable_vertical = false}},
 	groups = {crumbly = 3, soil = 1, spreading_dirt_type = 1},
 	drop = 'aether_dirt',
@@ -28,7 +28,7 @@ minetest.register_node("aether2:aether_dirt_with_grass", {
 --Stones
 minetest.register_node("aether2:holystone", {
 	description = "Holystone",
-	tiles = {"holystone.png"},
+	tiles = {"blocks/holystone.png"},
 	groups = {aetherstone = 1},
 })
 
@@ -36,7 +36,7 @@ minetest.register_node("aether2:holystone", {
 
 minetest.register_node("aether2:skyroot_log", {
 	description = "skyroot_log",
-	tiles = {"skyroot_log_top.png", "skyroot_log_top.png", "skyroot_log.png"},
+	tiles = {"blocks/skyroot_log_top.png", "blocks/skyroot_log_top.png", "blocks/skyroot_log.png"},
 	paramtype2 = "facedir",
 	is_ground_content = false,
 	groups = {aehtertree = 1},
@@ -45,7 +45,7 @@ minetest.register_node("aether2:skyroot_log", {
 
 minetest.register_node("aether2:golden_oak_log", {
 	description = "Golden Oak Log",
-	tiles = {"golden_oak_log_top.png", "golden_oak_log_top.png", "golden_oak_log.png"},
+	tiles = {"blocks/golden_oak_log_top.png", "blocks/golden_oak_log_top.png", "blocks/golden_oak_log.png"},
 	paramtype2 = "facedir",
 	is_ground_content = false,
 	groups = {aehtertree=2 },
@@ -67,8 +67,8 @@ minetest.register_node("aether2:skyroot_leaves", {
   description = "Skyroot Leaves",
   drawtype = "allfaces_optional",
   waving = 1,
-  tiles = {"skyroot_leaves.png"},
-  special_tiles = {"skyroot_leaves_simple.png"},
+  tiles = {"sblocks/kyroot_leaves.png"},
+  special_tiles = {"blocks/skyroot_leaves_opaque.png"},
   paramtype = "light",
   is_ground_content = false,
   groups = {snappy = 3, leafdecay = 3, leaves = 1},
@@ -78,8 +78,8 @@ minetest.register_node("aether2:golden_oak_leaves", {
   description = "Golden Oak Leaves",
   drawtype = "allfaces_optional",
   waving = 1,
-  tiles = {"golden_oak_leaves.png"},
-  special_tiles = {"golden_oak_leaves.png"},
+  tiles = {"blocks/golden_oak_leaves.png"},
+  special_tiles = {"blocks/golden_oak_leaves_opaque.png"},
   paramtype = "light",
   is_ground_content = false,
   groups = {snappy = 3, leafdecay = 3, leaves = 1},
@@ -88,7 +88,7 @@ minetest.register_node("aether2:golden_oak_leaves", {
 
 minetest.register_node("aether2:skyroot_planks", {
   description = "Skyroot Planks",
-  tiles = {"skyroot_planks.png"},
+  tiles = {"blocks/skyroot_planks.png"},
   groupts = {choppy=1, oddly_breakable_by_hand=1, snappy = 1,},
 })
 
@@ -97,9 +97,9 @@ minetest.register_node("aether2:ambrosium_torch", {
   description = "Ambrosium Torch",
   groups = {oddly_breakable_by_hand=1},
   drawtype = "mesh",
-  mesh = "ambrosium_torch_floor.obj",
-  inventory_image = "ambrosium_torch.png",
-  wield_image = "ambrosium_torch.png",
+  mesh = "mbrosium_torch_floor.obj",
+  inventory_image = "blocks/ambrosium_torch.png",
+  wield_image = "blocks/ambrosium_torch.png",
   tiles = {{
         name = "ambrosium_torch_animated.png",
         animation = {type = "vertical_frames", aspect_w = 16, aspect_h = 16, length = 3.3}
